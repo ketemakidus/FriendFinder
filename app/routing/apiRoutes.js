90b0cb4a38f4920a9b7beb781d9e20ba0036ee8d
kidus bitew <ketemakidus009@gmail.com>
2019-02-28 08:31:15 -0500

    finder

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,18 +2,12 @@ var surveydata = require("../data/friends");
 
 module.exports = function(app) {
 
-// to get the data 
  app.get("./api/friends", function(req,res){
    res.sendFile(__dirname + "/survey")
    res.sendFile("survey.html");
    res.json(surveydata);
  });
 
-
-
-
-
-	// Add new friend entry
 	app.post('/api/friends', function(req, res) {
 
     surveydata.push(req.body)
