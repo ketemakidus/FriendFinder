364eee4355a6e133ba0b9f70e5f34b4d2e09b00f
kidus bitew <ketemakidus009@gmail.com>
2019-02-28 03:06:04 -0500

    finder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var newfriends= [
   {
-    name:"Ahmed",
+    Name:"Ahmed",
     photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     scores:[
         5,
@@ -18,7 +18,7 @@ var newfriends= [
 
   {
 
-  name:"kiki",
+  Name:"kiki",
   photo:"https://www.google.com/search?q=kiki&rlz=1C1CHBF_enUS823US823&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiMs-Gpys7gAhWFGt8KHSA2Cm8Q_AUIDygC&biw=1455&bih=717#",
   scores:[1,
     5,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,14 +4,21 @@ module.exports = function(app) {
 
 // to get the data 
  app.get("./api/friends", function(req,res){
+   res.sendFile(__dirname + "/survey")
+   res.sendFile("survey.html");
    res.json(surveydata);
  });
 
-// to post useer info
- app.post("/api/friends", function(req,res){
-   surveydata.push(req.body)
-   res.json(surveydata);
- });
-}
+	app.post('/api/friends', function(req, res) {
 
+    surveydata.push(req.body)
 
+		res.json(surveydata);
+		
+		var newfriend = req.body;
+
+		var userResponses = newfriend.scores;
+
+		var matchName = '';
+		var matchImage = '';
+		
