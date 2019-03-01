cb8bc67a6dc84de3a8fcc77e75a6130935a03fe6
kidus bitew <ketemakidus009@gmail.com>
2019-03-01 15:52:43 -0500

    finder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var newfriends= [
   {
       Name:"Ahmed",
-      photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.kikishop.fr%2F427%2Fpetit-kiki-articule.jpg&imgrefurl=https%3A%2F%2Fsokolscout277.weebly.com%2Fblog%2Fsee-kiki-full-movie&docid=Qcu9LyUO4707VM&tbnid=c0Ns-8egMvXr7M%3A&vet=10ahUKEwiOmebkieDgAhURPN8KHS1_AUEQMwhiKAIwAg..i&w=2276&h=2588&bih=757&biw=1600&q=kiki&ved=0ahUKEwiOmebkieDgAhURPN8KHS1_AUEQMwhiKAIwAg&iact=mrc&uact=8",
+      photo: "https://www.kikishop.fr/427/petit-kiki-articule.jpg",
       scores:[
         5,
         1,
@@ -19,7 +19,7 @@ var newfriends= [
   {
 
   Name:"kiki",
-  photo:"https://www.google.com/search?q=kiki&rlz=1C1CHBF_enUS823US823&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiMs-Gpys7gAhWFGt8KHSA2Cm8Q_AUIDygC&biw=1455&bih=717#",
+  photo:"https://www.kikishop.fr/427/petit-kiki-articule.jpg",
   scores:[1,
     5,
     5,
